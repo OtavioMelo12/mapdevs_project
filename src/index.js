80fd427891a750ea2ad4bbde3b4c42e5652b17a4
OtavioMelo12 <otavio.souza@estudante.ufla.br>
2020-01-14 15:33:43 -0300

    feat: finished backend
    Finished all backend codes

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://mapuser:mapuser@cluster0-p9zi7.mongodb.net/mapdevs?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('useCreateIndex', true);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+// Métodos HTTP: GET, POST, PUT, DELETE
+
+// Tipos de Parâmetros:
+// Query Params: req.query (filtros, ordenacao, paginacao, ...)
+// Route Params: req.params (identificar um recurso na alteracao ou remocao)
+// Body Params: req.body (dados para criacao ou alteracao de um registro)
+
+// MongoDB (nao-relacional)
