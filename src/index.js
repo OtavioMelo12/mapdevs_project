e84d05c6cbbfd08e46110a6ebef42e14a74dc6a6
OtavioMelo12 <otavio.souza@estudante.ufla.br>
2020-01-15 17:05:39 -0300

    feat: add cors
    Add package cors to allow acess to backend

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -11,6 +12,7 @@ mongoose.connect('mongodb+srv://mapuser:mapuser@cluster0-p9zi7.mongodb.net/mapde
 
 mongoose.set('useCreateIndex', true);
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
