0c45467a0fefce765e6d9b43f2ade68b2ff9eba5
OtavioMelo12 <otavio.souza@estudante.ufla.br>
2020-01-15 15:42:31 -0300

    feat: finished interface
    finished basic interface and responsive

--- src/App.js
+++ src/App.js
@@ -1,28 +1,82 @@
+// componente: um bloco isolado de html, css e js
+// propriedade: informacoes que um componente pai passa para o componente filho
+// estado: informacoes mantidas pelo componente
+
 import React from 'react';
-import logo from './logo.svg';
+
+import './globalStyle.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/49327595?s=460&v=4" alt="Otávio Melo" />
+              <div className="user-info">
+                <strong>Otávio Melo</strong>
+                <span>ReactJS, Node, Python</span>
+              </div>
+            </header>
+            <p>Apaixonado por novos aprendizados!</p>
+            <a href="https://github.com/OtavioMelo12">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/49327595?s=460&v=4" alt="Otávio Melo" />
+              <div className="user-info">
+                <strong>Otávio Melo</strong>
+                <span>ReactJS, Node, Python</span>
+              </div>
+            </header>
+            <p>Apaixonado por novos aprendizados!</p>
+            <a href="https://github.com/OtavioMelo12">Acessar perfil do Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/49327595?s=460&v=4" alt="Otávio Melo" />
+              <div className="user-info">
+                <strong>Otávio Melo</strong>
+                <span>ReactJS, Node, Python</span>
+              </div>
+            </header>
+            <p>Apaixonado por novos aprendizados!</p>
+            <a href="https://github.com/OtavioMelo12">Acessar perfil do Github</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
